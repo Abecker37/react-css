be755638a3261f12aa555769da8eb7627a1c875d
Abecker37 <72383676+Abecker37@users.noreply.github.com>
2023-09-25 11:05:48 +0200

    initialisation

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import Card from './components/card'
+import './App.scss'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        <Card></Card>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Card.jsx
@@ -0,0 +1,25 @@
+function Card() {
+    return (
+        <>
+            <div className="container">
+                <div className='header'>
+                    <img src="https://cdn.dashfight.com/29d194d5ba0f694f3ccb37e652ad19a77761f63e_480.png" alt="" />
+                </div>
+                <div>
+                    <h1>Zero Suit Samus</h1>
+                    <h2>Description</h2>
+                    <div className="para">
+                        <p>
+                            Although galactic bounty hunter Samus Aran is most recognizable in her red-and-yellow Power Suit,
+                            in one of her earliest missions her ship was shot down following a harrowing escape from planet Zebes, 
+                            and her armor was destroyed. As Samus continued her battle against the Space Pirates, she stealthily infiltrated her foes'
+                            mothership and proved that she had the ability to come out on top, even without raw firepower.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Card
